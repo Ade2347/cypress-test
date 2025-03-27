641761d3d3c72cc270a6ca2b80c674e53c435438
adeniyi <adeniyi@surveycto.com>
2025-03-28 00:59:21 +0100

    dropdown list

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -123,19 +123,31 @@ describe('First test suite', () => {
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
 
-        // let date = new Date()
-        // date.setDate(date.getDate() + 5)
-        // console.log(date)
-
+        let date = new Date()
+        date.setDate(date.getDate() + 1)
+        let futureDay = date.getDate()
+        let futureMonth = date.toLocaleDateString('en-US', {month: 'short'})
+        let futureYear = date.getFullYear()
+        let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
 
         cy.contains('nb-card','Common Datepicker').find('input').then(input => {
             cy.wrap(input).click()
-            cy.get('.day-cell').not('.bounding-month').contains('21').click()
-            cy.wrap(input).invoke('prop', 'value').should('contain', 'Mar 21, 2025')
-            cy.wrap(input).should('have.value', 'Mar 21, 2025')
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if(!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)){
+                    cy.get('[data-name="chevron-right"').click()
+                }else{
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+            })
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
+            cy.wrap(input).should('have.value', dateToAssert)
         })
-  
+    })
+    it.only('drop down', () => {
+        cy.get('nav').find('nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav').find('nb-select').should('contain', 'Dark')
+        
 
     })
-
 })
